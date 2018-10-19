3b2f5b5aaa28bf40945650be347031b8b86c6859
Nick Manning <nickmanning214@gmail.com>
2018-10-19 00:31:33 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// app.js
+
+import InlineEditor from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+
+InlineEditor
+    .create( document.querySelector( '#editor' ), {
+        plugins: [ Essentials, Paragraph, Bold, Italic ],
+        toolbar: [ 'bold', 'italic' ]
+    } )
+    .then( editor => {
+        console.log( 'Editor was initialized', editor );
+    } )
+    .catch( error => {
+        console.error( error.stack );
+    } );
