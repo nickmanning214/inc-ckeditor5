7e66d39eedaff76c01138e0138d58ea15b1e6f76
Nick Manning <nickmanning214@gmail.com>
2018-11-13 16:13:16 -0500

    apparently I have to do this again

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ window.IncCKEditorFeatures = {
     icons,
     GetEditorOnButtonPress,
     version:require('./package.json').version,
-    currentVersion:'0.0.2'
+    currentVersion:'0.0.3'
 }
 
 
